da2a8a2f34e3238fea3d672ee4eafd514793757a
noahfranco <noahfra@gmail.com>
2019-08-15 12:11:53 -0400

    starting over

--- reducer-project/src/components/TodoList.js
+++ reducer-project/src/components/TodoList.js
@@ -1,22 +1,2 @@
-import React from "react"; 
 
-const TodoList = ({todos, toggleCompleted, clearCompleted}) => {
-    return( 
-        <>
-    <button className="clear-btn" onClick={clearCompleted}> Clear </button>
-    <div>
-      {todos.map(i => {
-        return <div 
-          className={"item " + (i.completed ? "done" : "")} 
-          key={i.id} 
-          onClick={() => toggleCompleted(i.id)}>
-          {i.item}
-        </div>
-      })}
-    </div>
-    </>
-    ); 
-}
-
-export default TodoList; 
 
