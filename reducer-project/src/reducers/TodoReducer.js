7b7ec3ee8abea113a2bd7bb2f836937bdfd85fff
noahfranco <noahfra@gmail.com>
2019-08-15 12:29:52 -0400

    set the initialState and reducer

--- reducer-project/src/reducers/TodoReducer.js
+++ reducer-project/src/reducers/TodoReducer.js
@@ -1,1 +1,42 @@
 
+export const initialState = {
+    todos: [
+      {
+        item: "Learn about reducers",
+        completed: false,
+        id: 3892987589
+      }
+    ]
+  };
+
+  export const reducer = (state, action) => {
+      console.log("action", action); 
+      switch (action.type) {
+          case "ADD_TODO"; 
+          return {
+              ...state,
+              todos: [...state.todos, action.payload]
+          };
+          case "TOGGLE_COMPLETE":
+            return {
+                ...state, 
+                todos: state.todos.map(todo => 
+                    todo.id === action.payload
+                    ? { ...todo, completed: !todo.completed }
+                    : todo
+                    )
+            };
+            case "CLEAR_COMPLETED":
+                return {
+                    ...state, 
+                    todos: state.todos.filter(todo => !todo.completed)
+                }; 
+                case "DELETE_TODO":
+                    return {
+                        ...state, 
+                        todos: state.todos.filter(todo => todo.id !== action.payload)
+                    }
+                    default: 
+                        return state; 
+      }
+  }
